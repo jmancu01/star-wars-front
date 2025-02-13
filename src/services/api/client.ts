1b5382652c51bf52ad4dd17ee0d3b20b76de5e7b
JuanMancuSidetool <juan.mancuso@sidetool.co>
2025-02-13 15:27:38 -0300

    add server env

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   headers: {
     "Content-Type": "application/json",
   },
